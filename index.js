d3ec8d941c010133cff6488a68a5888651156af3
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-09-03 17:26:44 -0500

    Agregando require del módulo path de node y usando método path.resolve

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
-//Módulos que contienen las funciones
+//Módulos de node
+const path = require('path');
+console.log(path);
+
+//Módulos internos que contienen las funciones
 const isMarkdown = require('./lib/ismd');
-// const readingFile = require('./lib/reading');
-// const identifingFile = require('./lib/identify');
-// const fileUrl = new URL('');
+const readingFile = require('./lib/reading');
+const identifingFile = require('./lib/identify');
 
 //Argumentos de la línea de comandos pasados por la CLI
 const listOfArgs = process.argv;
-const fileToRead = listOfArgs[2];
-console.log(fileToRead);
+const fileToRead = listOfArgs[2]; //Este es el path
+const absolutePath = path.resolve(fileToRead);
+console.log(absolutePath);
 
-// module.exports = (fs) => {
+
+// module.exports = () => {
+
 // };
-
