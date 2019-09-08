99cab990f3e526df600171809b1a30ffc722b2f0
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-09-08 18:37:17 -0500

    Leyendo el contenido del archivo que se pasa por la CLI como path

--- index.js
+++ index.js
@@ -3,23 +3,30 @@
 const path = require('path');
 //Hacer require del file system "fs" de node.js
 const fs = require('fs');
-// console.log(fs);
 
 //Módulos internos que contienen las funciones
 const mdFile = require('./lib/ismd');
 const readingFile = require('./lib/readingLinks');
 const identifingFile = require('./lib/getLinks');
 
-//Variables
-// const arrAbsolutePath = absolutePath.split();
+//Argumentos recibidos en la CLI
+const listOfArgs = process.argv; //Usar process.argv de node para leer el path
+
+const fileToRead = listOfArgs[2]; //Este es el path (posicion de la ruta del archivo)
+// console.log(typeof(fileToRead));
+
+const absolutePath = path.resolve(fileToRead);
+console.log(typeof (absolutePath));
+const finalPath = absolutePath.toString();
+console.log(finalPath);
 
 //Invocaciones
-mdFile(path);
-readingFile(fs);
+mdFile(finalPath);
+readingFile(finalPath);
 
 //Módulo de entrada a la aplicación (aquí se invocan módulos para pasarles argumentos)
 module.exports = (path) => {
-    
+
 };
 
 
